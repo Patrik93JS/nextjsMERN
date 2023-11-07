b343af5659bcd69558082436c655cc18ba3d3994
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-11-07 18:05:47 +0100

    uprava

--- nextjs-mern/app/(root)/layout.tsx
+++ nextjs-mern/app/(root)/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
 			<html lang="en">
 				<body className={inter.className}>
 					<Topbar />
-					{children}
 
 					<main className="flex flex-row">
 						<LeftSidebar />
