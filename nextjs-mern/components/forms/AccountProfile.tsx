e248fa497f555bdde6050a956ee701dc7ee12cf1
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-11-08 18:08:26 +0100

    priprava formulare

--- nextjs-mern/components/forms/AccountProfile.tsx
+++ nextjs-mern/components/forms/AccountProfile.tsx
@@ -15,6 +15,9 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { UserValidation } from "@/lib/validations/user";
 import * as z from "zod";
+import Image from "next/image";
+import { ChangeEvent } from "react";
+import { Textarea } from "@/components/ui/textarea";
 
 interface Props {
 	user: {
@@ -41,7 +44,16 @@ const AccountProfile = ({
 		},
 	});
 
-	function onSubmit(values) {
+	const handleImage = (
+		e: ChangeEvent,
+		fieldChange: (value: string) => void
+	) => {
+		e.preventDefault();
+	};
+
+	function onSubmit(
+		values: z.infer<typeof UserValidation>
+	) {
 		// Do something with the form values.
 		// ✅ This will be type-safe and validated.
 		console.log(values);
@@ -51,24 +63,101 @@ const AccountProfile = ({
 		<Form {...form}>
 			<form
 				onSubmit={form.handleSubmit(onSubmit)}
-				className="space-y-8"
+				className="flex flex-col justify-start gap-10"
 			>
+				<FormField
+					control={form.control}
+					name="profile_photo"
+					render={({ field }) => (
+						<FormItem className="flex items-center gap-4">
+							<FormLabel className="account-form_image-label">
+								{field.value ? (
+									<Image
+										src={field.value}
+										alt="profile_photo"
+										width={96}
+										height={96}
+										priority
+										className="rounded-full object-contain"
+									/>
+								) : (
+									<Image
+										src="/assets/profile.svg"
+										alt="profile_photo"
+										width={24}
+										height={24}
+										className=" object-contain"
+									/>
+								)}
+							</FormLabel>
+							<FormControl className="flex-1 text-base-semibold text-gray-200">
+								<Input
+									type="file"
+									accept="image/*"
+									placeholder="Upload a photo"
+									className="account-form_image-input"
+									onChange={(e) =>
+										handleImage(e, field.onChange)
+									}
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+
+				<FormField
+					control={form.control}
+					name="name"
+					render={({ field }) => (
+						<FormItem className="flex items-center gap-3 w-full">
+							<FormLabel className="text-base-semibold text-light-2">
+								Name
+							</FormLabel>
+							<FormControl className="flex-1 text-base-semibold text-gray-200">
+								<Input
+									type="text"
+									className="account-form_input no-focus"
+									{...field}
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+
 				<FormField
 					control={form.control}
 					name="username"
 					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Username</FormLabel>
-							<FormControl>
+						<FormItem className="flex items-center gap-3 w-full">
+							<FormLabel className="text-base-semibold text-light-2">
+								Username
+							</FormLabel>
+							<FormControl className="flex-1 text-base-semibold text-gray-200">
 								<Input
-									placeholder="shadcn"
+									type="text"
+									className="account-form_input no-focus"
 									{...field}
 								/>
 							</FormControl>
-							<FormDescription>
-								This is your public display name.
-							</FormDescription>
-							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<FormField
+					control={form.control}
+					name="bio"
+					render={({ field }) => (
+						<FormItem className="flex items-center gap-3 w-full">
+							<FormLabel className="text-base-semibold text-light-2">
+								Bio
+							</FormLabel>
+							<FormControl className="flex-1 text-base-semibold text-gray-200">
+								<Textarea
+									rows={10}
+									className="account-form_input no-focus"
+									{...field}
+								/>
+							</FormControl>
 						</FormItem>
 					)}
 				/>
